57a497fd9d1aba85bf0ed36f11a1e22899907d4e
alexandrenavarro <alexandrenavarro@gmail.com>
2018-09-24 09:23:35 -0300

    create activity BookTableAcitivity

---
+++ app/src/main/java/br/com/alexandrenavarro/booktable/BookTableActivity.kt
@@ -0,0 +1,12 @@
+package br.com.alexandrenavarro.booktable
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BookTableActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_table)
+    }
+}
